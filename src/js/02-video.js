2bc4a96914484d38cb1106c9820bc5005fa1caa1
rlmgtr <rlmagtira@gmail.com>
2024-04-13 11:15:24 +0800

    hw2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Player(iframe);
+
+const timeFunction = data => {
+    console.log(data);
+};
+
+player.on('timeupdate', throttle(timeFunction, 1000));
